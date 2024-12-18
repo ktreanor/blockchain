32b82871a055f9b2f820a61d2cea5c9928386dc4
Kevin Treanor <kevin.treanor@icloud.com>
2024-12-18 10:22:25 -0500

    Updated the dunder methods

--- block_chain.py
+++ block_chain.py
@@ -16,15 +16,24 @@ class BlockChain:
         for block in self.__chain:
             yield block
 
-    def __getitem__(self, key) -> Block:
+    def __getitem__(self, key: int) -> Block:
         """
-        Dunder method to allow items in the Blockchain to be retrieved by an indexed
-        :param key: The index of the list
-        :return: The block at the given index
+        Dunder method that allows for blocks to be referenced by index within the blockchain
+
+        :param key: Index of the block in the chain you wish to reference
+        :type key: int
+        :return: The block at the index given in the blockchain
         """
         return self.__chain[key]
 
-    def __add_genesis_block(self) -> None:
+    def __len__(self) -> int:
+        """
+
+        :return: Length of the blockchain
+        """
+        return len(self.__chain)
+
+    def __append_genesis(self) -> None:
         """
         Creates the first (genesis) block in a new blockchain
         :return: None
@@ -32,28 +41,41 @@ class BlockChain:
         genesis_block = Block(0, "Genesis Block", "0" * 64)
         self.__chain.append(genesis_block)
 
-    def add_block(self, data):
+    def append(self, data):
 
+        # If this is an empty blockchain a genesis block must be created first
         if len(self.__chain) == 0:
-            self.__add_genesis_block()
+            self.__append_genesis()
 
-        block_number = self.__chain[-1].block_number + 1
+        index = self.__chain[-1].index + 1
         prev_hash = self.__chain[-1].block_hash
 
-        new_block = Block(block_number, data, prev_hash)
+        new_block = Block(index, data, prev_hash)
         self.__chain.append(new_block)
 
-    def validate_chain(self) -> bool:
-        pass
+    def validate(self):
 
-    def store_blockchain(self, file_name: str):
+        chain_valid = True
 
-        # Pickling the Blockchain
+        for index, block in enumerate(self.__chain):
+            if index != block.index:
+                chain_valid = False
+
+    def save(self, file_name: str):
+        """
+
+        :param file_name:
+        :return:
+        """
         with open(file_name + '.dat', 'wb') as f:
             pickle.dump(self.__chain, f)
 
-    def load_blockchain(self, file_name: str):
+    def read(self, file_name: str):
+        """
 
+        :param file_name:
+        :return:
+        """
         with open(file_name + '.dat', 'rb') as f:
             self.__chain = pickle.load(f)
 
@@ -62,17 +84,18 @@ def main():
     test_block_chain = BlockChain()
 
     """
-    test_block_chain.add_block("Block Number 1")
-    test_block_chain.add_block("Block number 2")
-    test_block_chain.add_block("Block number 3")
+    test_block_chain.append("Block Number 1")
+    test_block_chain.append("Block number 2")
+    test_block_chain.append("Block number 3")
 
-    test_block_chain.store_blockchain('test_chain')
+    test_block_chain.save('test_chain')
     """
-
-    test_block_chain.load_blockchain('test_chain')
+    test_block_chain.read('test_chain')
 
     for block in test_block_chain:
         print(block)
+
+    print(f'Blockchain Length {len(test_block_chain)}')
 
 
 
