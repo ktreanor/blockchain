4d5a9f26549051588b742e470a112eaca95ae466
Kevin Treanor <kevin.treanor@icloud.com>
2024-12-18 10:21:59 -0500

    Removed the hash as a class member and made it calculate everytime it's needed

--- block.py
+++ block.py
@@ -2,48 +2,59 @@ from datetime import datetime
 import block_utilities
 
 class Block:
-    __block_number: int
-    __data: str
-    __nonce: int
-    __block_hash: str
+
+    # Block header
+    __index: int
     __previous_hash: str
     __timestamp: datetime
+    __nonce: int
 
-    def __init__(self, block_number: int, data: str, previous_hash: str) -> None:
+    # Block payload
+    __data: str
+
+    def __init__(self, index: int, data: str, previous_hash: str) -> None:
         """
-        Creates a block instance, the block number, data, and the previous block's hash is needed
+        Creates a new instance of a block
 
-        :param block_number: The number of the new block being created
-        :param data: The data load of the block
+        :param index: The index of block
+        :type index: int
+        :param data: The data payload
+        :type data: str
         :param previous_hash: The hash value of the previous block
+        :type previous_hash: str
         """
-        # TODO remove block number from block since this should always be the same as the index in the chain
-        self.__block_number = block_number
+
+        self.__index = index
         self.__data = data
         self.__previous_hash = previous_hash
 
-        proof_of_work = block_utilities.mine(block_number, data, previous_hash)
+        proof_of_work = block_utilities.mine(index, data, previous_hash)
         self.__nonce = proof_of_work[0]
-        self.__block_hash = proof_of_work[1]
         self.__timestamp = proof_of_work[2]
-        # TODO Do I actually need to store the hash in the block or should it be created each time?
 
-    def __str__(self):
+        # Note: The hash of the block is calculated each time it's needed, the only hash actually stored is the previous
+
+    def __str__(self) -> str:
+        """
+        Representation of the block as a string, this allows the block to be printed, viewed etc.
+
+        :return: A string representation of the block
+        """
         return (
-            f'Block Number: {self.__block_number}\n'
+            f'Index: {self.__index}\n'
             f'Previous Hash: {self.previous_hash}\n'
             f'Data: {self.__data}\n'
             f'Nonce: {self.__nonce}\n'
-            f'Hash: {self.__block_hash}\n'
+            f'Hash: {self.hash}\n'
             f'Timestamp: {self.__timestamp}\n'
         )
 
     @property
-    def block_number(self) -> int:
+    def index(self) -> int:
         """
         Returns the block number
         """
-        return self.__block_number
+        return self.__index
 
     @property
     def data(self) -> str:
@@ -60,11 +71,13 @@ class Block:
         return self.__nonce
 
     @property
-    def block_hash(self) -> str:
+    def hash(self) -> str:
         """
         Returns the block hash
         """
-        return self.__block_hash
+
+        # The hash is calculated each time it's needed
+        return block_utilities.hash_block(self.__index, self.__data, self.__previous_hash, self.__nonce)
 
     @property
     def previous_hash(self) -> str:
@@ -80,30 +93,11 @@ class Block:
         """
         return self.__timestamp
 
-    def is_valid(self) -> bool:
-        """
-        Checks to see if the block is internally valid, meaning the hash of the block matches what is stored, and both are following the difficulty requirements
-        :return: Is the block internally valid
-        :rtype: bool
-        """
-
-        block_hash = block_utilities.hash_block(self.__block_number, self.__data, self.__previous_hash, self.__nonce)
-
-        # Insure the hashed block matches what is stored
-        if block_hash != self.__block_hash:
-            return False
-
-        # Insure the hash follows the defined difficulty
-        if block_hash[:block_utilities.DIFFICULTY] != block_utilities.PROOF * block_utilities.DIFFICULTY:
-            return False
-
-        return True
 
 def main():
     test_block = Block(0, "Genesis Block", "0" * 64)
 
     print(test_block)
-    print(test_block.is_valid())
 
 if __name__ == "__main__":
     main()
